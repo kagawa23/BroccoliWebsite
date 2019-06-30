36ba80ab5d827358ee2f9c56de00e5cffeb755f3
pro <zczhaozc>
2019-06-30 09:38:46 +0800

    eslint prettier的配置

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { PureComponent } from "react";
-import "./app.scss";
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import './app.scss';
 
 class Test extends PureComponent {
+  static propTypes = {
+    message: PropTypes.string.isRequired
+  };
+
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return <div>Hello worlds {this.props.message}</div>;
+    const { message } = this.props;
+    return (
+      <div>
+        Hello worlds
+        {message}
+      </div>
+    );
   }
 }
 
